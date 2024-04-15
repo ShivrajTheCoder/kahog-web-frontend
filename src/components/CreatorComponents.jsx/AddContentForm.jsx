6ed0689ab3661ba2f319977ccb6677dbdd990744
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-15 14:11:04 +0530

    creator-upload-added

--- src/components/CreatorComponents.jsx/AddContentForm.jsx
+++ src/components/CreatorComponents.jsx/AddContentForm.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Input from '../Input';
+import Textarea from '../Textarea';
 
 export default function AddContentForm() {
   const [contentType, setContentType] = useState('video'); // Initial content type
-  const [interest, setInterest] = useState('');
   const [description, setDescription] = useState('');
   const [title, setTitle] = useState('');
+  const [mediaFile, setMediaFile] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [category, setCategory] = useState('');
+  const [error, setError] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [catError, setCatError] = useState(null);
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    setError(null);
+
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/category/getallcategories`);
+        if (response.status === 200) {
+          setCategories(response.data.categories);
+        } else {
+          setCatError("Couldn't fetch categories");
+        }
+      } catch (error) {
+        console.error(error);
+        setCatError("Failed to fetch categories"); // Provide a more informative error message
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     switch (name) {
       case 'contentType':
         setContentType(value);
-        break;
-      case 'interest':
-        setInterest(value);
         break;
       case 'description':
         setDescription(value);
@@ -26,20 +55,51 @@ export default function AddContentForm() {
     }
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Handle form submission logic here
-    console.log('Submitted content:', {
-      contentType,
-      interest,
-      description,
-      title,
-    });
-    // Reset form after submission (optional)
-    setContentType('video');
-    setInterest('');
-    setDescription('');
-    setTitle('');
+  const handleMediaFileChange = (e) => {
+    const selectedMediaFile = e.target.files[0];
+    setMediaFile(selectedMediaFile);
+  };
+
+  const handleThumbnailChange = (e) => {
+    const selectedThumbnail = e.target.files[0];
+    setThumbnail(selectedThumbnail);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Validate form fields
+    if (!title || !description || !mediaFile || !thumbnail || !category) {
+      setError('All fields are required');
+      return;
+    }
+    // Determine if the media file is a video
+    const isVideo = mediaFile.type.startsWith('video/');
+    // Submit form data
+    const formData = new FormData();
+    formData.append('name', title);
+    formData.append('description', description);
+    formData.append('mediaFile', mediaFile);
+    formData.append('thumbnail', thumbnail);
+    formData.append('categoryId', category);
+    formData.append('isVideo', isVideo);
+    formData.append('authorId', 2);
+    try {
+      const response = await axios.post(`${apiUrl}/podcasts/uploadpodcast`, formData);
+      console.log(response);
+      if (response.status === 201) {
+        // Reset form after successful submission
+        setTitle('');
+        setDescription('');
+        setMediaFile(null);
+        setThumbnail(null);
+        setCategory('');
+        setError('');
+      } else {
+        console.log("Something went wrong!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -59,53 +119,78 @@ export default function AddContentForm() {
           <option value="audio">Audio</option>
         </select>
       </div>
-      <div className="flex flex-col">
-        <label htmlFor="title" className="mb-2 text-gray-700">
-          Title:
-        </label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={title}
-          onChange={handleInputChange}
-          placeholder="Enter content title"
-          className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="interest" className="mb-2 text-gray-700">
-          Interest:
-        </label>
-        <input
-          type="text"
-          id="interest"
-          name="interest"
-          value={interest}
-          onChange={handleInputChange}
-          placeholder="Enter content interest"
-          className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="description" className="mb-2 text-gray-700">
-          Description:
-        </label>
-        <textarea
-          id="description"
-          name="description"
-          value={description}
-          onChange={handleInputChange}
-          placeholder="Describe your content"
-          rows={4}
-          className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
+      <Input
+        label="Title"
+        type="text"
+        id="title"
+        name="title"
+        value={title}
+        onChange={handleInputChange}
+        placeholder="Enter title"
+        error={error}
+      />
+      <Textarea
+        label="Description"
+        id="description"
+        name="description"
+        value={description}
+        onChange={handleInputChange}
+        placeholder="Enter description"
+        error={error}
+      />
+      <label htmlFor="mediaFile" className="block text-gray-700">
+        Choose Media File (Audio or Video):
+      </label>
+      <input
+        type="file"
+        id="mediaFile"
+        name="mediaFile"
+        accept="audio/*,video/*"
+        onChange={handleMediaFileChange}
+      />
+      <label htmlFor="thumbnail" className="block text-gray-700">
+        Choose Thumbnail:
+      </label>
+      <input
+        type="file"
+        id="thumbnail"
+        name="thumbnail"
+        accept="image/*"
+        onChange={handleThumbnailChange}
+      />
+      {loading ? (
+        <p>Loading categories...</p>
+      ) : (
+        <>
+          {categories.length > 0 ? (
+            <div className="flex flex-col">
+              <label htmlFor="category" className="text-gray-700">
+                Select Category:
+              </label>
+              <select
+                id="category"
+                name="category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                <option value="">Select Category</option>
+                {categories.map((cat) => (
+                  <option key={cat.id} value={cat.id}>{cat.category_name}</option>
+                ))}
+              </select>
+            </div>
+          ) : (
+            <p>No categories available</p>
+          )}
+        </>
+      )}
+      {error && <p className="text-red-500">{error}</p>}
       <button
         type="submit"
-        className="bg-green-500 w-fit text-white font-bold py-2 px-4 rounded-md"
+        className="bg-black w-fit text-white py-2 px-4 rounded-md"
       >
-        Submit Content
+        Upload Podcast
       </button>
     </form>
   );
