bf7d4e1a34791fb17184ab6b9540c8b21308d377
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-17 14:51:43 +0530

    login-style-changed

--- src/screens/Auth/Login.jsx
+++ src/screens/Auth/Login.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 import { adminLogin } from '../../redux/reducers/adminReducer';
 import { setDataWithExpiry } from '../../utils/localstore';
 import { useNavigate } from 'react-router-dom';
-import Input from '../../components/Input';
+import homiee from "../../assets/homepage/homiee.jpg";
 
 export default function AdminLogin() {
   const apiUrl = import.meta.env.VITE_API_URL;
@@ -64,7 +64,7 @@ export default function AdminLogin() {
         const { token, admin } = response.data;
         console.log(response);
         const { id, username } = admin;
-        const userId=id;
+        const userId = id;
         dispatch(adminLogin({ token, userId }));
         setDataWithExpiry("admin", { token, userId, creatorId: 1 }, 5);
         navigate("/dashboard");
@@ -92,30 +92,43 @@ export default function AdminLogin() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form onSubmit={handleSubmit} className="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <h2 className="text-2xl font-semibold mb-4 text-center">Admin Login</h2>
-        <Input
-          type="email"
-          name="email"
-          placeholder="Enter Email"
-          value={values.email}
-          onChange={handleChange}
-          error={errors.email}
-        />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          value={values.password}
-          onChange={handleChange}
-          error={errors.password}
-        />
-        {errors.message && <p className="text-red-500 mb-4 text-center">{errors.message}</p>}
-        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Login
-        </button>
-      </form>
+    <div className=" h-screen bg-blue-300 flex justify-center items-center">
+      <div className='h-2/3 w-2/3 grid grid-cols-2 '>
+        <img src={homiee} alt="Homiee" className=" h-full w-full" />
+        <form onSubmit={handleSubmit} className="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 ">
+          <h2 className="text-2xl font-semibold mb-4 text-center">Admin Login</h2>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter Email"
+              value={values.email}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded-full w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+            {errors.email && <p className="text-red-500 text-xs italic">{errors.email}</p>}
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter Password"
+              value={values.password}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded-full w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+            {errors.password && <p className="text-red-500 text-xs italic">{errors.password}</p>}
+          </div>
+          {errors.message && <p className="text-red-500 mb-4 text-center">{errors.message}</p>}
+          <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
